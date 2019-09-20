83d607940905461875862ddf6d438653cef3a7e3
Yaakov Azat <yaakovazat@gmail.com>
2019-09-20 20:59:14 +0600

    added copyright information to the package

--- azt.py
+++ azt.py
@@ -1,11 +1,14 @@
 import click
 from pyfiglet import Figlet
 
-f = Figlet()
-azt_welcome = f.renderText('AzatAI')
+
+azt_copyright = '©Azat Artificial Intelligence, LLP\nAl-Farabi Kazakh National University'
 
 
 @click.command()
 @click.version_option()
 def cli():
+    f = Figlet()
+    azt_welcome = f.renderText('AzatAI')
     click.secho(azt_welcome, fg='blue')
+    click.echo(azt_copyright)
