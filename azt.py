2d23e934260d5180196757b72d5e2a4fa62c6fec
Yaakov Azat <yaakovazat@gmail.com>
2019-09-20 19:48:48 +0600

    one command

--- azt.py
+++ azt.py
@@ -1,26 +1,15 @@
 import click
+from pyfiglet import Figlet
+
 
 @click.group()
-@click.version_option()
 def cli():
-    """ ~ Welcome to the Future!
-
-    This is the first azt cli test, we gonna work on this hard staff maybe for a long while....
-    """
+    pass
 
 
-@cli.group()
-def install():
-    """
-    Install ~azt packages
-    """
+@click.command()
+def init():
+    click.secho('init')
 
 
-@install.command()
-@click.argument('pkg')
-def install_pkg(pkg):
-    """
-    Install an ~azt package from ~azt package index.
-    """
-    click.secho("Installing ~azt package:{}".format(pkg))
-
+cli.add_command(init)
--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='azt',
-    version='0.0.1',
-    py_modules=['azt'],
+    version='0.0.2',
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'click',
+        'pyfiglet',
     ],
     entry_points='''
         [console_scripts]
