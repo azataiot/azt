8ee0f5cb79bb463656bb5008e585d4b8f4440592
Yaakov Azat <yaakovazat@gmail.com>
2020-02-09 21:23:14 +0600

    modified:   azt.py

--- azt.py
+++ azt.py
@@ -1,6 +1,20 @@
 import click
-from packages import branding
+from packages import branding, github
 import settings
+
+
+# Start the CLI here.
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()  # @cli, not @click!
+def git():
+    click.secho('Running the ~azt Github utils:..')
+    github.git()
+
 
 if __name__ == '__main__':
     branding.run()
+    cli()
